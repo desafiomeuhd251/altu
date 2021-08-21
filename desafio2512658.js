720db11c17a6214c3af70fa554ffedf29f9c9616
desafiomeuhd251 <seuemail>
2021-08-21 12:00:04 -0300

    Versao002

--- desafio2512658.js
+++ desafio2512658.js
@@ -2,7 +2,7 @@ function TestaChave(){
     var inSenha = document.getElementById("inChave");
     var senha = inSenha.value;
     if (senha=="23/06/1912"){
-        alert ("Chave correta !"); // chamar outra pagina, ou alerta ou codigo
+        window.location.replace('nivel0012512658.html');
     } else{
         alert ("Chave incorreta !");
     }
