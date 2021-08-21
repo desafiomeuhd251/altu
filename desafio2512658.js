3cd1cb39e03e0843c6e1118c5ac6d2a8e329ddd1
desafiomeuhd251 <seuemail>
2021-08-21 19:49:09 -0300

    Versao005

--- desafio2512658.js
+++ desafio2512658.js
@@ -1,6 +1,7 @@
 function TestaChave(){
     var inSenha = document.getElementById("inChave");
     var senha = inSenha.value;
+    // HUMM Espertinho , venho olhar diretamente no código, ganhará muito tempo com isso ! 
     if (senha=="23/06/1912"){
         window.location.replace('nivel0012512658.html');
     } else{
